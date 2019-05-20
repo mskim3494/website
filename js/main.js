a84f4c80582349eda61b9aba1298416e273dae10
Min Su Kim <mskim3494@Mins-MacBook-Pro.local>
2019-05-20 17:20:09 -0500

    menu changes

--- js/main.js
+++ js/main.js
@@ -6,27 +6,62 @@ $(window).load(function() {
 });
 
 let current = 'hero';
+let long = false;
 
 function fadeToHome(){
-	$(`#${current}`).fadeOut('fast');
-	current = 'hero';
-	$(`#${current}`).fadeIn('slow');
+	if (long == false) {
+		$(`#${current}`).fadeOut('fast');
+		current = 'hero';
+		$(`#${current}`).fadeIn('slow');
+	} else {
+		document.getElementById('top').scrollIntoView({behavior: "smooth"});
+	}
 }
 
 function fadeToAbout(){
-	$(`#${current}`).fadeOut('fast');
-	current = 'about';
-	$(`#${current}`).fadeIn('slow');
+	if(long == false){
+		$(`#${current}`).fadeOut('fast');
+		current = 'about';
+		$(`#${current}`).fadeIn('slow');
+	} else {
+		document.getElementById('about').scrollIntoView({behavior: "smooth"});
+	}
 }
 
 function fadeToResume(){
-	$(`#${current}`).fadeOut('fast');
-	current = 'resume';
-	$(`#${current}`).fadeIn('slow');
+	if(long == false){
+		$(`#${current}`).fadeOut('fast');
+		current = 'resume';
+		$(`#${current}`).fadeIn('slow');
+	} else {
+		document.getElementById('resume').scrollIntoView({behavior: "smooth"});
+	}
 }
 
 function fadeToContact(){
-	$(`#${current}`).fadeOut('fast');
-	current = 'contact';
-	$(`#${current}`).fadeIn('slow');
+	if (long == false) {
+		$(`#${current}`).fadeOut('fast');
+		current = 'contact';
+		$(`#${current}`).fadeIn('slow');
+	} else {
+		document.getElementById('contact').scrollIntoView({behavior: "smooth"});
+	}
+}
+
+function changeStyle(){
+	if(long == false) {
+		$(`#${current}`).fadeOut('fast');
+		$('#change-style span').text('Switch Pages');
+		$('#hero').fadeIn('fast');
+		$('#resume').fadeIn('fast');
+		$('#about').fadeIn('fast');
+		$('#contact').fadeIn('fast');
+	} else {
+		$('#change-style span').text('Switch Long');
+		$('#resume').fadeOut('fast');
+		$('#about').fadeOut('fast');
+		$('#contact').fadeOut('fast');
+	}
+	long = !long;
+	$("i", "#change-style").toggleClass("fa-bars fa-file");
 }
